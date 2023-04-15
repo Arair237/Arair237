19943881545ec784aa9409d36769e22284dc691c
wuko233 <wuko233@gmail.com>
2023-04-15 22:18:24 +0800

    index changed

--- test.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request,render_template
-
-app = Flask(__name__)
-@app.route('/', methods = ['get'])
-def get():
-    return render_template("index.html")
-
-app.run(host = "127.0.0.1", port = 28288, debug = 'Ture')
