12b14908155c3b9bb7c69bf4d568276cda888a95
wuko233 <wuko233@gmail.com>
2023-07-16 11:37:35 +0800

    fixed ctrl+c issue

--- static/LangToCN.js
+++ static/LangToCN.js
@@ -1,9 +1,7 @@
-<script type="text/javascript">
-  var userLang = navigator.language || navigator.userLanguage;
+var userLang = navigator.language || navigator.userLanguage;
   if (userLang == "zh-CN") {
     var answer = window.confirm("你想要访问中文版本吗?");
     if (answer) {
       window.location.href = "/";
     }
   }
-</script>
