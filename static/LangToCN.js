c8afb421fd0d3b0d087d099cc2d61837c5e305c1
wuko233 <wuko233@gmail.com>
2023-08-12 18:10:53 +0800

    remove js

--- static/LangToCN.js
+++
@@ -1,7 +0,0 @@
-var userLang = navigator.language || navigator.userLanguage;
-  if (userLang == "zh-CN") {
-    var answer = window.confirm("你想要访问中文版本吗?");
-    if (answer) {
-      window.location.href = "/";
-    }
-  }
--- static/LangToEN.js
+++
@@ -1,7 +0,0 @@
-var userLang = navigator.language || navigator.userLanguage;
-  if (userLang != "zh") {
-    var answer = window.confirm("Do you want to visit the English version of this website?");
-    if (answer) {
-      window.location.href = "/en";
-    }
-  }
